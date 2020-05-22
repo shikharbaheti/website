bb8af1670aa46e6ed2831eb453bbda35f8c80e84
Shikhar Baheti <shikharbaheti@gmail.com>
2020-05-22 17:16:00 -0500

    Updates

---
+++ precache-manifest.d023c4bd9855e9f2260399e3c000b8b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2e0af30ee15834423c3c9cf0daee5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fea535afd68ba9327ad",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "639afb531f01c16380db",
+    "url": "/static/css/main.f4c430d0.chunk.css"
+  },
+  {
+    "revision": "0fea535afd68ba9327ad",
+    "url": "/static/js/2.37c4056b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.37c4056b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639afb531f01c16380db",
+    "url": "/static/js/main.18f6c66a.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/instagram.0e84d592.png"
+  },
+  {
+    "revision": "9fa9df9480695ad88c5e453c219036f3",
+    "url": "/static/media/myPhoto.9fa9df94.jpg"
+  }
+]);
